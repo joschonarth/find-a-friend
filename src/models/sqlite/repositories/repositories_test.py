e616b64eee23b4a7e4cf64a36574712eeace8ecc
joschonarth <joschonarth@gmail.com>
2025-01-02 19:49:06 -0300

    feat: create pets repository and add method to list all pets

---
+++ src/models/sqlite/repositories/repositories_test.py
@@ -0,0 +1,12 @@
+import pytest
+from src.models.sqlite.settings.connection import db_connection_handler
+from .pets_repository import PetsRepository
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="interaction with the database")
+def test_list_pets():
+    repo = PetsRepository(db_connection_handler)
+    response = repo.list_pets()
+    print()
+    print(response)
