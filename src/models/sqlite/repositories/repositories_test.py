bceaf4bb6d716063b2b58938d62d8248f7e0ac76
joschonarth <joschonarth@gmail.com>
2025-01-03 19:19:31 -0300

    feat: create people repository and add method to insert person

--- src/models/sqlite/repositories/repositories_test.py
+++ src/models/sqlite/repositories/repositories_test.py
@@ -1,6 +1,7 @@
 import pytest
 from src.models.sqlite.settings.connection import db_connection_handler
 from .pets_repository import PetsRepository
+from .people_repository import PeopleRepository
 
 db_connection_handler.connect_to_db()
 
@@ -16,3 +17,13 @@ def test_delete_pet():
     name = "luna"
     repo = PetsRepository(db_connection_handler)
     repo.delete_pets(name)
+
+@pytest.mark.skip(reason="interaction with the database")
+def test_insert_person():
+    first_name = "first"
+    last_name = "last"
+    age = 20
+    pet_id = 1
+
+    repo = PeopleRepository(db_connection_handler)
+    repo.insert_person(first_name, last_name, age, pet_id)
