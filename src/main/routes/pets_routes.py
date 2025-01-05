54078ed8ad60a890f137efae530ef611c608ab2a
joschonarth <joschonarth@gmail.com>
2025-01-05 19:57:25 -0300

    feat: implement list pets route

--- src/main/routes/pets_routes.py
+++ src/main/routes/pets_routes.py
@@ -1,7 +1,15 @@
 from flask import Blueprint, jsonify
+from src.view.http_types.http_request import HttpRequest
+
+from src.main.composer.pet_lister_composer import pet_lister_composer
 
 pet_route_bp = Blueprint("pets_routes", __name__)
 
 @pet_route_bp.route("/pets", methods=["GET"])
 def list_pets():
-    return jsonify({ "message": "Olá Mundo" }), 200
+    http_request = HttpRequest()
+    view = pet_lister_composer()
+
+    http_response = view.handle(http_request)
+
+    return jsonify(http_response.body), http_response.status_code
